6fef47e97e4ed6bf6ace34f9a033414b9e9554c6
Neil Thawani <neilthawani@gmail.com>
2018-07-22 14:02:49 -0500

    Bit of deletion

---
+++ app/components/multi-chat/users-list/component.js
@@ -0,0 +1,22 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+
+});
+
+// var usersList = getElement('.users-list');
+// var numbersOfUsers = getElement('.numbers-of-users');
+//
+// numbersOfUsers.innerHTML = 1;
+//
+// var usersContainer = getElement('.users-container');
+//
+// usersList.onclick = function() {
+//     if (usersList.className.indexOf('selected') != -1) {
+//         usersList.className = usersList.className.replace( / selected/g , '');
+//         usersContainer.style.display = 'none';
+//     } else {
+//         usersList.className += ' selected';
+//         usersContainer.style.display = 'block';
+//     }
+// };
